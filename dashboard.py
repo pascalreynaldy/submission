33b742712382eeece903d1dc10d6294e8c7e1bb3
pascalreynaldy <138509132+pascalreynaldy@users.noreply.github.com>
2024-10-11 11:46:17 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -10,8 +10,8 @@ from sklearn.preprocessing import StandardScaler
 st.title('Analisis Data Peminjaman Sepeda - Bike Sharing')
 
 # Load day.csv and hour.csv datasets
-day_df = pd.read_csv('../data/day.csv')
-hour_df = pd.read_csv('../data/hour.csv')
+day_df = pd.read_csv('day.csv')
+hour_df = pd.read_csv('hour.csv')
 
 # Convert 'dteday' column to datetime format
 day_df['dteday'] = pd.to_datetime(day_df['dteday'])
